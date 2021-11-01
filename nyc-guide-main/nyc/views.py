641c5d7fe56143362bdfbd71fea56b1820b08601
Kayla Ousley <kayousley22@gmail.com>
2021-10-31 22:31:11 -0400

    added venue links to activities page

--- nyc-guide-main/nyc/views.py
+++ nyc-guide-main/nyc/views.py
@@ -16,7 +16,7 @@ def borough(request, borough):
 
 def activity(request, activity, borough):
     if request.method == 'GET':
-        return render(request=request, template_name='activity.html', context={'activity': activity, 'activities': boroughs[borough].keys()})
+        return render(request=request, template_name='activity.html', context={'venues': boroughs[borough][activity].keys(), 'activity': activity, 'activities': boroughs[borough].keys()})
 # ! need to inplement venues as in the urls.py
 # ! first get the venue names to render on the activities page.
 # ! create a url that takes each venue to it's own page
